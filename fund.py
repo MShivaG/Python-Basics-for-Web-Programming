cb68301fce4f0b582f1172e0947dbca0487f23c0
MShivaG <shivaganesheee@gmail.com>
2020-11-27 11:00:34 +0530

    FinalCommit

---
+++ fund.py
@@ -0,0 +1,17 @@
+def square(x):
+    return x * x
+
+for i in range(10):
+    print(f"The square of {i} is {square(i)}")
+
+""" Output:
+The square of 0 is 0
+The square of 1 is 1
+The square of 2 is 4
+The square of 3 is 9
+The square of 4 is 16
+The square of 5 is 25
+The square of 6 is 36
+The square of 7 is 49
+The square of 8 is 64
+"""
---
+++ loop.py
@@ -0,0 +1,12 @@
+# Create a list:
+names = ["Harry", "Ron", "Hermione"]
+
+# Print each name:
+for name in names:
+    print(name)
+
+""" Output:
+Harry
+Ron
+Hermione
+"""
