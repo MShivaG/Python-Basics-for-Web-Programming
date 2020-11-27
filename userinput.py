0ca67bdf0ec0601c0166842141c1921a7f032349
MShivaG <shivaganesheee@gmail.com>
2020-11-27 09:48:03 +0530

    User input and formatted strings

---
+++ userinput.py
@@ -0,0 +1,3 @@
+#input() is the built in function in python to accept user values
+a = int(input("Enter a number:")) # To get a number input int() is used to convert the string input to integer
+print(f"The number you have entered is {a}") #by using f before quotes python provides a feature of formatted strings
